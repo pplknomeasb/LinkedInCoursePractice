fb523cb572ce1a95df879f9742865886352382ae
pplknomeasb <thompson.brennan@gmail.com>
2021-07-13 10:37:42 -0400

    Updating with new codewars problem.  An Isogram is a statement that has no repeating characters in a string.  This version currently isnt working.

---
+++ src/IsogramRunner.java
@@ -0,0 +1,55 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class IsogramRunner {
+
+    public static boolean checkIsogram(String str){
+        //Creates an array as long as the alphabet
+        //All index's are false values
+
+        String toLowerCase = str.toLowerCase(Locale.ROOT);
+
+        int counter = 0;
+
+        //Assuming the statement is an Isogram
+        boolean isIsogram = true;
+
+        int index = 0;
+
+        for(int i = 0; i < str.length(); i++){
+            for(int j = 0; j < str.length(); i++){
+                if(toLowerCase.charAt(j) == toLowerCase.charAt(i)){
+                    counter += 1;
+                }
+                if(counter > 1){
+                    isIsogram = false;
+                    break;
+
+                    }
+
+                }
+            counter = 0;
+            }
+
+
+
+
+
+        return isIsogram;
+    }
+    public static void main(String[] args) {
+
+        System.out.println("This is a codewars problem.");
+
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.nextLine();
+
+        boolean isogramCheck;
+
+        isogramCheck = checkIsogram(str);
+
+        System.out.println(isogramCheck);
+
+    }
+}
