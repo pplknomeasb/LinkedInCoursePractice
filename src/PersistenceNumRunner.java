17d2762d2e9d6eb81ad19a560c7c60afda4bc6e9
pplknomeasb <thompson.brennan@gmail.com>
2021-07-14 11:56:33 -0400

    Updating with new codewars problem.  Persistence is the science of a process that outlives the                                      data that created it.  This program takes in a Long value                                      and multiplies the numbers at each index to create a new                                      value which is lesser than the original number, unless the                                      number has a length of one.  If the result of the original                                      function has a length of more than 1, the process is repeated                                      until the number results with a once character value.

---
+++ src/PersistenceNumRunner.java
@@ -0,0 +1,15 @@
+public class PersistenceNumRunner {
+    public static Long persistenceCheck(Long n){
+
+        Long num = 0L;
+
+
+        do {
+        }while(n.toString().length() > 1 );
+
+        return num;
+    }
+    public static void main(String[] args) {
+
+    }
+}
