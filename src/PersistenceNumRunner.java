4a2520d69d0c49028e8efed3c00a6195ed4eab5f
pplknomeasb <thompson.brennan@gmail.com>
2021-07-15 15:30:36 -0400

    Attempting to create an array of long values.  the code breaks because the value within the array is hashcode and not the value.

--- src/PersistenceNumRunner.java
+++ src/PersistenceNumRunner.java
@@ -1,36 +1,57 @@
+import com.sun.jdi.LongValue;
+
 import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class PersistenceNumRunner {
     public static Long persistenceCheck(Long n){
 
-        var count = 0L;
+
+
         var multiplier=n.toString().length();
-        BigDecimal valueBig;
-        var resultBig = new BigDecimal(1);
-        var nToString = n.toString().split("");
-
-
-
-
-            if(multiplier<=1){
-                //If the strings length has no multipliers, count remains 0
+        Long count = 0L;
+        Long result = 1L;
+        Long[] numbersSplit = {n.longValue()};
+        System.out.println(numbersSplit);
+        do {
+            if(multiplier<=1) {
+                break;
             }else{
+                count +=1;
+                for(int i = 0; i<=multiplier; i++){
+                    result = result * numbersSplit[LongValue(i)];
+                    multiplier = result.toString().length();
+                }
 
-                do {
+            }
+        }while(multiplier<=1);
 
-                    for (int i = 0; i < multiplier; i++) {
-                        var valueBig = new BigDecimal(nToString[i]).multiply(resultBig);
-                        count += 1;
-                    }
-                    multiplier = valueBig.toString().length();
-                }while(multiplier>2);
+        return count;
+
+
+//        if(multiplier<=1){
+//            //If the strings length has no multipliers, count remains 0
+//        }else{
+//
+//            do {
+//
+//                BigDecimal valueBig;
+//                var resultBig = new BigDecimal(1);
+//                var nToString = n.toString().split("");
+//                var count = 0L;
+//
+//                    for (int i = 0; i < multiplier; i++) {
+//                        var valueBig = BigDecimal(nToString[i]).multiply(resultBig);
+//                        count += 1L;
+//                    }
+//                    multiplier = valueBig.toString().length();
+//                }while(multiplier>2);
 //                do{
 //                    var nToString = n.toString().split("");
 //                    System.out.println(nToString);
 //
 //                }while();
-            }
+//            }
 
 
 
@@ -44,7 +65,7 @@ public class PersistenceNumRunner {
 //
 //        }while(num.toString().length() > 1 );
 
-        return count;
+//        return count;
     }
     public static void main(String[] args) {
         System.out.println("This is a codewars problem");
